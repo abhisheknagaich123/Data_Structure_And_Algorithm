0c35d97cc26cc2726ab9543e482c0e25151bb753
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2022-04-21 21:28:51 +0530

    BST

--- src/Binary_Search_Tree.java
+++ src/Binary_Search_Tree.java
@@ -9,31 +9,20 @@ public class Binary_Search_Tree {
     }
 
 }
-    static Node createTree(){
-        Node root=null;
-        System.out.println(" Enter data : ");
-        int data=sc.nextInt();
-        if (data==-1){
-            return null;
+    static Node insert(Node root,int key){
+
+         if (root == null) {
+             root = new Node(key);
+             return root;
+         }
+        if (key< root.data){
+//            System.out.println(" Enter data for left : " +data);
+            root.left=insert(root.left,key);
         }
-        root=new Node(data);
-        System.out.println(" Enter data for left : " +data);
-        root.left=createTree();
-        System.out.println(" Enter data for right : "+data);
-        root.right=createTree();
+        else if (key> root.data){
+//            System.out.println(" Enter data for right : " +data);
+            root.right=insert(root.right,key);
+        }
         return root;
-    }
-    static Scanner sc =new Scanner(System.in);;
-    public static void main(String[] args) {
-
-        createTree();
-//        inOrder(root);
-//        System.out.println( );
-//        preOrder(root);
-//        System.out.println( );
-//        postOrder(root);
-        //    heightofbinary(root);
-
-
     }
 }
